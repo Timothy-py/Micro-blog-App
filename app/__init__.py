9a42d1a2f658ffe7c2b39a466a6f3fc6b6b78bff
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-26 19:07:34 +0100

    learnt about forms in flask and created a  login page for the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,5 @@ from .config import Config
 
 appInstance = Flask(__name__)
 appInstance.config.from_object(Config)
+
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import appInstance
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+from .forms import LoginForm
 
 
 @appInstance.route('/index')
@@ -16,3 +17,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, post=post)
+
+
+@appInstance.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for(endpoint='index'))
+    return render_template('login.html', title='Sign In', form=form)
+
