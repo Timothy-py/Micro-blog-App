732b2424c84d922f7a764ccd2b7e03d50ed10baf
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-26 15:18:37 +0100

    added learnt about adding templates to a flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
+from .config import Config
+
 
 appInstance = Flask(__name__)
-
-from app import routes
+appInstance.config.from_object(Config)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import appInstance
+from flask import render_template
 
 
 @appInstance.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'BLOGGERS'}
+    post = [
+        {
+            'author': {'username': 'Timothy'},
+            'body': 'It\'s a brand new day!'
+        },
+        {
+            'author': {'username': 'Obadan'},
+            'body': 'It\'s a brand new Moon!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, post=post)
