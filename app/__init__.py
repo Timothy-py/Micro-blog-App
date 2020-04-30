68b157d493c3d59473f6609761b1f3b0ba3e910d
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-30 10:57:53 +0100

    created database migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
 
 
 appInstance = Flask(__name__)
 appInstance.config.from_object(Config)
+db = SQLAlchemy(app=appInstance)
+migrate = Migrate(app=appInstance, db=db)
 
-from app import routes
+from app import routes, models
--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database setup (for sqlite)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 class Config(object):
